0aa6c45764e678b1db0353d3b2e28608b7251f18
Lasseignejk <jaye914@hotmail.com>
2023-05-02 00:20:29 -0400

    added login and notes to app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,15 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import SignUp from "./components/SignUp";
+import Login from "./components/Login";
+import MyNotes from "./components/MyNotes";
 
 function App() {
 	return (
 		<Routes>
 			<Route path="/signup" element={<SignUp />} />
+			<Route path="/login" element={<Login />} />
+			<Route path="/mynotes" element={<MyNotes />} />
 		</Routes>
 	);
 }
--- server/routes/noteRoutes.js
+++ server/routes/noteRoutes.js
@@ -2,6 +2,16 @@ const express = require("express");
 const router = express.Router();
 const { Notes } = require("../models");
 
+router.get("/all_notes", async (req, res) => {
+	const { userId } = req.body;
+	const notes = await Notes.findAll({
+		where: {
+			userId: userId,
+		},
+	});
+	res.send(notes);
+});
+
 router.post("/new_note", async (req, res) => {
 	const { contents, userId } = req.body;
 
@@ -11,6 +21,20 @@ router.post("/new_note", async (req, res) => {
 		createdAt: new Date(),
 		updatedAt: new Date(),
 	});
+	res.send(note);
+});
+
+router.post("/update_note", async (req, res) => {
+	const { contents, id } = req.body;
+
+	const note = await Notes.update(
+		{ contents: contents },
+		{
+			where: {
+				id: id,
+			},
+		}
+	);
 	res.send(note);
 });
 
